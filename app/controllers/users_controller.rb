dd2f9bf021ef8137b05eae056517a8df9438a45e
a <a>
2017-04-20 00:50:24 +0000

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.page(params[:page])
   end
-  
+
   def show
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  root to: 'toppages#index'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
